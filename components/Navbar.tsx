3054e81fca3182896c1c4bf5a17440fd4cd47895
Yuvraj <devyuvraj883@gmail.com>
2024-09-07 14:42:20 +0530

    Created Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <span>KS Online Classes</span>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Team</li>
+      </ul>
+    </nav>
+  )
+}
