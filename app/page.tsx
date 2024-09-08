31c66ec68577dc9c6530fd61fd39194c52fce105
Yuvraj <devyuvraj883@gmail.com>
2024-09-08 10:18:00 +0530

    Added Services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import ExcelSection from "@/components/ExcelSection";
 import HeroSection from "@/components/HeroSection";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <HeroSection/>
     <About/>
     <ExcelSection/>
+    <Services/>
     </>
   );
 }
