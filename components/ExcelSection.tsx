2ee6f9ba71cfd0e2cfc1d1238267f2acae42fb45
Yuvraj <devyuvraj883@gmail.com>
2024-09-08 12:06:44 +0530

    Added responsiveness

--- components/ExcelSection.tsx
+++ components/ExcelSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ExcelSection() {
   return (
     <div className='bg-[url("/images/excel3.jpg")] h-[600px] max-w-screen bg-cover p-8 my-8'>
-      <div className='flex flex-col justify-center h-full text-black w-[60%] bg-white bg-opacity-50 p-8 rounded-lg'>
+      <div className='flex flex-col justify-center h-full text-black w-[100%] md:w-[60%] bg-white bg-opacity-50 p-8 rounded-lg'>
         <h1 className='text-8xl font-bold'>Excel In Your Exams</h1>
         <p className='font-semibold text-lg mt-4'>Join our online classes and boost your results.</p>
       </div>
